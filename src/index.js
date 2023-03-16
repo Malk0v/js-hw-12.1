a05e4e399a37a8a0b30185b132fcf1d0f3b1817f
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-03-16 14:23:41 +0200

    add components

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const API_KEY = '1207c88ea12a40948c6a5f2f9fe75304';
+
+const input = document.querySelector('.input-js');
+const start = document.querySelector('.start-js');
+
+input.addEventListener('submit', onSearch);
+start.addEventListener('click',onSearch);
+
+function onSearch(e) {
+    e.preventDefault();
+    
+    const SearchNews = input.value;
+
+    fetch(
+        `https://newsapi.org/v2/everything?q=${SearchNews}&apiKey=${API_KEY}`
+    ).then(response => response.json())
+      .then(data => {
+        const headlines = data.articles
+          .map(article => {
+              return `<div class="news">
+        <h2><span>Тема: </span>${article.title}</h2> 
+        <li>${article.description}
+        <hr>
+        Источник - ${article.author}</li>
+        </div>`;
+          })
+          .join('');
+        document.querySelector(
+          '.c'
+        ).innerHTML = `<ul>${headlines}</ul>`;
+      });
+};
